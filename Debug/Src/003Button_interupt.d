977b10b69590af51d1b8b22f40b25c41b5dc6c05
hiepvo2001 <84091503+hiepvo2001@users.noreply.github.com>
2025-05-14 23:34:40 +0700

    • Configure AFIO_MAPR register  - Config remap of SPI1,I2C1,UART1,UART2,UART3,  TIM1,TIM2,TIM3,TIM4,CAN,PD01,TIM5CH4, ADC1_ETRGINJ  ADC1_ETRGREG,ADC2_ETRGINJ,ADC2_ETRGREG  - Config SWJ_CFG  - add macros posible values of all above  - add API for control all above

--- Debug/Src/003Button_interupt.d
+++
@@ -1,7 +0,0 @@
-Src/003Button_interupt.o: ../Src/003Button_interupt.c \
- C:/STM32_course/Project1/stm32f1xx_drivers/drivers/Inc/stm32f103xx_gpio_driver.h \
- C:/STM32_course/Project1/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h \
- C:/STM32_course/Project1/stm32f1xx_drivers/drivers/Inc/stm32f103xx_gpio_driver.h
-C:/STM32_course/Project1/stm32f1xx_drivers/drivers/Inc/stm32f103xx_gpio_driver.h:
-C:/STM32_course/Project1/stm32f1xx_drivers/drivers/Inc/stm32f103xx.h:
-C:/STM32_course/Project1/stm32f1xx_drivers/drivers/Inc/stm32f103xx_gpio_driver.h:
